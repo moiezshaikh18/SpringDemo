f4e67194440cda2c82c914829f5b70e8721f51df
mueej <v-mueej@fieldera.com>
2025-01-27 17:14:38 +0530

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context;
+
+        Alien alien = new Alien();
+        alien.code();
+    }
+}
